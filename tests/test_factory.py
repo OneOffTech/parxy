b9c5d6cd33bb34ef9fb841c4fa071592bd999f5f
Alessio Vertemati <alessio@oneofftech.xyz>
2026-01-20 19:20:47 +0100

    wip: check why driver instantion fails in ci

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -97,10 +97,6 @@ class TestDriverFactory:
         driver = DriverFactory.build().driver('pdfact')
         assert isinstance(driver, PdfActDriver)
 
-    @pytest.mark.skipif(
-        os.getenv('GITHUB_ACTIONS') == 'true',
-        reason='External service required, skipping tests in GitHub Actions.',
-    )
     def test_llamaparse_driver_instantiated(self):
         DriverFactory.reset()
         driver = DriverFactory.build().driver('llamaparse')
@@ -116,10 +112,6 @@ class TestDriverFactory:
         driver = DriverFactory.build().driver('unstructured_local')
         assert isinstance(driver, UnstructuredLocalDriver)
 
-    @pytest.mark.skipif(
-        os.getenv('GITHUB_ACTIONS') == 'true',
-        reason='External service required, skipping tests in GitHub Actions.',
-    )
     def test_landingai_driver_instantiated(self):
         DriverFactory.reset()
         driver = DriverFactory.build().driver('landingai')
